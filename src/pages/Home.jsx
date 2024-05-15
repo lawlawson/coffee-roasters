e3b1d9c480105e5062319ef07c65c68a1e4c2b1f
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-05-15 23:48:13 +0100

    basic text on homepage header section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import Header from '../components/Header';
+
 function Home() {
   return (
     <>
-      <h1 className='text-4xl font-bold underline'>Home</h1>
+      <Header />
+      <h1 className='text-4xl font-bold underline'>
+        Great coffee made simple.
+      </h1>
+      <p>
+        Start your mornings with the world&apos;s best coffees. Try our expertly
+        curated artisan coffees from our best roasters delivered directly to
+        your door, at your schedule.
+      </p>
+      <button>Create your plan</button>
     </>
   );
 }

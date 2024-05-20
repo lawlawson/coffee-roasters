bb2c4493e74dad0d832ad63e470eef02fa24d3cb
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-05-20 23:28:00 +0100

    styling of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 const Header = () => {
   return (
-    <div className="bg-[url('/src/assets/home/desktop/image-hero-coffeepress.jpg')]">
-      <h1 className='text-4xl font-bold underline'>
-        Great coffee made simple.
-      </h1>
-      <p>
-        Start your mornings with the world&apos;s best coffees. Try our expertly
-        curated artisan coffees from our best roasters delivered directly to
-        your door, at your schedule.
-      </p>
-      <button>Create your plan</button>
+    <div className='flex justify-center items-center'>
+      <div className="bg-[url('/src/assets/home/desktop/image-hero-coffeepress.jpg')] bg-no-repeat bg-center bg-cover p-6 rounded w-5/6">
+        <div className='container'>
+          <h1 className='text-8xl font-bold text-white'>
+            Great coffee made simple.
+          </h1>
+          <p className='text-white text-xl'>
+            Start your mornings with the world&apos;s best coffees. Try our
+            expertly curated artisan coffees from our best roasters delivered
+            directly to your door, at your schedule.
+          </p>
+          <button className='bg-white p-3 rounded mt-5'>
+            Create your plan
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
